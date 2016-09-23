60679fd86d079537c2b267464903a1d9664e19dc
Alexander Gorishnyak <kefir500@gmail.com>
2016-09-23 14:49:12 +0600

    Handle Unicode release titles properly [fixes #1]

--- ghstats.py
+++ ghstats.py
@@ -223,9 +223,10 @@ def get_release_downloads(release, quiet=False):
     """
     downloads_total = 0
     if not quiet:
+        title = release["name"].encode(sys.stdout.encoding, errors='ignore')
         published = time.strptime(release["published_at"], "%Y-%m-%dT%H:%M:%SZ")
         print("")
-        print("          --- " + _Text.HEADER + release["name"] + _Text.END + " ---\n")
+        print("          --- " + _Text.HEADER + title + _Text.END + " ---\n")
         print("         Tag: " + release["tag_name"])
         print("      Author: " + release["author"]["login"])
         print("         URL: " + _Text.UNDERLINE + release["html_url"] + _Text.END)
