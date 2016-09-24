1525e3dd91f64a0603d8b96382a106699e09b815
Alexander Gorishnyak <kefir500@gmail.com>
2016-09-24 20:46:49 +0600

    Add Python package setup files

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from os import path
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst')) as f:
+    readme = f.read()
+
+setup(
+    name='ghstats',
+    version='1.1.0',
+    packages=['ghstats'],
+    description='GitHub Release download count and other statistics.',
+    long_description=readme,
+    author='Alexander Gorishnyak',
+    author_email='kefir500@gmail.com',
+    license='MIT',
+    url='https://github.com/kefir500/ghstats',
+    keywords='github release download count stats statistics',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities'
+    ]
+)
